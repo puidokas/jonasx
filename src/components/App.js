e3ebac1a9d215445c2bd63a8fd74662588a4b0cb
Jonas Hoffmann <jonashoffmann@outlook.dk>
2018-12-03 22:25:06 +0100

    fixed tooltips

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { throttle } from "throttle-debounce";
 import '../styles.css';
-import Header from './Header'
-import {IntlProvider} from "react-intl";
-import { addLocaleData } from "react-intl";
+// react-intl
+import {IntlProvider, addLocaleData} from "react-intl";
 import locale_en from 'react-intl/locale-data/en';
 import locale_da from 'react-intl/locale-data/da';
 import lang_da from "../translations/da.json";
 import lang_en from "../translations/en.json";
 import {FormattedMessage} from 'react-intl';
+// components
+import Header from './Header'
 import NavBar from './NavBar';
 
 class App extends Component {
@@ -54,6 +55,8 @@ class App extends Component {
         
         let language = this.state.lang;
         let messages = language === 'da' ? lang_da : lang_en;
+        let li_badge_container = actResponsive ? " flex-wrap" : "";
+        let li_badge = actResponsive ? " mb-2" : "";
 
         addLocaleData([...locale_en, ...locale_da]);
 
@@ -72,11 +75,11 @@ class App extends Component {
                     <FormattedMessage id="nav.code">{h => <h2 id="code"><a href="#top">{h}</a></h2>}</FormattedMessage>
                     <FormattedMessage id="code">{t => <p>{t} <a href={this.content.git_url} rel="noopener noreferrer" target='_blank'>GitLab</a>.</p>}</FormattedMessage>
                     <FormattedMessage id="nav.contact">{h => <h2 id="contact"><a href="#top">{h}</a></h2>}</FormattedMessage>
-                    <div className={"d-flex" + (actResponsive ? " flex-wrap" : "")}>
+                    <div className={"d-flex" + li_badge_container}>
                         <div className="flex-grow-1 text-justify mr-4">
                             <p><FormattedMessage id="contact" values={{link: <a href="https://www.linkedin.com/in/hoffmannjonas/" rel="noopener noreferrer" target='_blank'>LinkedIn</a>}}/>.</p>
                         </div>  
-                        <div className={"LI-profile-badge" + (actResponsive ? " my-3" : "")} data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="hoffmannjonas"><a className="LI-simple-link" href='https://dk.linkedin.com/in/hoffmannjonas?trk=profile-badge'>Jonas Hoffmann</a></div>
+                        <div className={"LI-profile-badge" + li_badge} data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="hoffmannjonas"><a className="LI-simple-link" href='https://dk.linkedin.com/in/hoffmannjonas?trk=profile-badge'>Jonas Hoffmann</a></div>
                     </div>
                 </div>
             </IntlProvider>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, injectIntl} from 'react-intl';
 import { FaGitlab, FaLinkedin, FaIdCard } from "react-icons/fa";
-import {injectIntl} from 'react-intl'; 
+import ReactTooltip from 'react-tooltip'
 
 class NavBar extends Component {
     render() {
         const { intl, content } = this.props;
+        let flexDirection, spacing, textCentered, marginAuto, navBarColor, navBarColorContainer;
 
-        let flexDirection, spacing, textCentered, marginAuto;
         if(this.props.actResponsive) {
+            navBarColorContainer = "";
+            navBarColor = " _navbar";
             flexDirection = " flex-column";
             spacing = "";
             textCentered = " text-center";
             marginAuto = "";
         } else {
+            navBarColorContainer = " _navbar";
+            navBarColor = "";
             flexDirection = " flex-row";
             spacing = " px-2";
             textCentered = "";
@@ -23,19 +27,22 @@ class NavBar extends Component {
         var lang_country = this.props.lang === "da" ? "gb" : "dk";
 
         return(
-            <div className={"_navbar d-flex mb-3" + flexDirection}>
-                <div className={"d-flex" + marginAuto + flexDirection + textCentered}>
+            <div className={"d-flex mb-3" + flexDirection + navBarColorContainer}>
+                {this.props.actResponsive && <b className="text-center mb-1">Navigation</b>}
+                <div className={"d-flex" + marginAuto + flexDirection + textCentered + navBarColor}>
                     <a className={spacing} href="#top"><FormattedMessage id="nav.intro"/></a>
                     <a className={spacing} href="#cv"><FormattedMessage id="nav.cv"/></a>
                     <a className={spacing} href="#code"><FormattedMessage id="nav.code"/></a>
                     <a className={spacing} href="#contact"><FormattedMessage id="nav.contact"/></a>
                 </div>
-                <div className={"d-flex" + flexDirection + textCentered}>
+                {this.props.actResponsive && <b className="text-center my-1">Quick links</b>}
+                <div className={"d-flex" + flexDirection + textCentered + navBarColor}>
                     <a href={intl.formatMessage({id: "cv_url"})} className={spacing} target="_blank" data-tip={intl.formatMessage({id: "quick_links.cv"})}><FaIdCard/></a>
                     <a href={content.git_url} className={spacing} target="_blank" data-tip={intl.formatMessage({id: "quick_links.code"})}><FaGitlab/></a>
                     <a href={content.linkedin_url} className={spacing} target="_blank" data-tip={intl.formatMessage({id: "quick_links.li"})}><FaLinkedin/></a>
-                    <a href="/" onClick={(e) => this.props.changeLang(e)} className={spacing}>
-                        <span className={"flag-icon flag-icon-" + lang_country + " " + spacing} data-tip={intl.formatMessage({id: "change_lang"})}/>
+                    <a href="/" onClick={(e) => this.props.changeLang(e)} className={spacing} data-for="change_lang" data-tip={intl.formatMessage({id: "change_lang"})}>
+                        <span className={"flag-icon flag-icon-" + lang_country + " " + spacing}/>
+                        <ReactTooltip id="change_lang" getContent={[() => { return intl.formatMessage({id: "change_lang"}) }]}/>
                     </a>
                 </div>
             </div>

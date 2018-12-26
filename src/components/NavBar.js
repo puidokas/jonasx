6647c9d27e5ca91667a1cc671a5e65599a8a8f76
Jonas Hoffmann <jonashoffmann@outlook.dk>
2018-12-26 21:44:11 +0100

    improved spacing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {FormattedMessage, injectIntl} from 'react-intl';
+import {injectIntl} from 'react-intl';
 import { FaGitlab, FaLinkedin, FaIdCard } from "react-icons/fa";
 
 class NavBar extends Component {
@@ -7,20 +7,18 @@ class NavBar extends Component {
         const { intl, content } = this.props;
 
         return(
-            <div>
-                <div className="navbar">
-                    <div>
-                        <b className="navName">Navigation</b>
-                        <a href="#cv"><FormattedMessage id="nav.cv"/></a>
-                        <a href="#code"><FormattedMessage id="nav.code"/></a>
-                        <a href="#contact"><FormattedMessage id="nav.contact"/></a>
-                    </div>
-                    <div>
-                        <b className="navName">{intl.formatMessage({id: "quick_links"})}</b>
-                        <a href={intl.formatMessage({id: "cv_url"})} target="_blank" data-tip={intl.formatMessage({id: "quick_links.cv"})}><FaIdCard/></a>
-                        <a href={content.git_url} target="_blank" data-tip={intl.formatMessage({id: "quick_links.code"})}><FaGitlab/></a>
-                        <a href={content.linkedin_url} target="_blank" data-tip={intl.formatMessage({id: "quick_links.li"})}><FaLinkedin/></a>
-                    </div>
+            <div className="navbar">
+                <div>
+                    <b className="navName">Navigation:</b>
+                    <a href="#cv">{intl.formatMessage({id: "nav.cv"})}</a>
+                    <a href="#code">{intl.formatMessage({id: "nav.code"})}</a>
+                    <a href="#contact">{intl.formatMessage({id: "nav.contact"})}</a>
+                </div>
+                <div>
+                    <b className="navName">{intl.formatMessage({id: "quick_links"})}:</b>
+                    <a href={intl.formatMessage({id: "cv_url"})} target="_blank" data-tip={intl.formatMessage({id: "quick_links.cv"})}><FaIdCard/></a>
+                    <a href={content.git_url} target="_blank" data-tip={intl.formatMessage({id: "quick_links.code"})}><FaGitlab/></a>
+                    <a href={content.linkedin_url} target="_blank" data-tip={intl.formatMessage({id: "quick_links.li"})}><FaLinkedin/></a>
                 </div>
             </div>
         );
